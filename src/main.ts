a4027ec94f07e374ae8c686e2520fda8633cb833
Kyle Andrews <codingwithkyle@gmail.com>
2021-03-10 13:25:52 -0500

    Refined UX & UI

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { render, html } from "lit-html";
 import SuperComponent from "@codewithkyle/supercomponent";
-import { diceware } from "./diceware";
+import diceware from "./diceware";
+import { refresh } from "tooltipper";
 
 type InputComponentDataModel = {
     value: string;
@@ -50,33 +51,36 @@ class InputComponent extends SuperComponent<InputComponentDataModel, InputCompon
         if(this.model.value.length === 5 && this.state === "INVALID" || this.model.value.length !== 5 && this.state === "VALID"){
             this.trigger("TOGGLE");
         }
-        if (this.model.value.length !== 5){
-            input.setCustomValidity("You must provide 5 numbers between the values of 1 and 6.");
-        } else if (this.checkNumbers()) {
-            input.setCustomValidity("Values must be between 1 and 6.");
-        } else {
-            input.setCustomValidity("");
+        if (!input.classList.contains("is-invalid")){
+            if (this.model.value.length !== 5){
+                input.setCustomValidity("You must provide 5 numbers between the values of 1 and 6.");
+            } else if (this.checkNumbers()) {
+                input.setCustomValidity("Values must be between 1 and 6.");
+            } else {
+                input.setCustomValidity("");
+            }
+            input.reportValidity();
         }
-        input.reportValidity();
     }
 
     private async getNumbers(){
+        let data = "";
         try {
             const request = await fetch("https://qrng.anu.edu.au/API/jsonI.php?length=10&type=uint16&size=1");
             const resonse = await request.json();
-            const data = `${resonse.data.join("")}`;
-            return data;
+            data = `${resonse.data.join("")}`;
         } catch (e){
-            let data = "";
             for (let i = 1; i <= 5; i++){
                 data += this.getRandomInt(1, 6);
             }
-            return data;
         }
+        return data;
     }
 
-    private generateNumbers:EventListener = () => {
-        const data = this.getNumbers();
+    private generateNumbers:EventListener = async () => {
+        const button = this.querySelector("button");
+        button.style.animation = "spin 600ms linear infinite";
+        const data = await this.getNumbers();
         const updatedModel = {...this.model};
         updatedModel.value = "";
         for (let i = 0; i < data.length; i++){
@@ -94,6 +98,7 @@ class InputComponent extends SuperComponent<InputComponentDataModel, InputCompon
                 updatedModel.value += `${this.getRandomInt(1, 6)}`;
             }
         }
+        button.style.animation = "none";
         this.update(updatedModel);
     }
 
@@ -105,9 +110,13 @@ class InputComponent extends SuperComponent<InputComponentDataModel, InputCompon
 
     render(){
         const view = html`
-            <div style="display:block;">
-                <input class="${this.state === "INVALID" ? "is-invalid" : "is-valid"}" required type="number" value="${this.model.value}" @input=${this.handleInput} @blur=${this.handleBlur}>
-                <button @click=${this.generateNumbers}>Roll for me</button>
+            <div class="input" flex="row nowrap items-center">
+                <input style="flex:1;" class="${this.state === "INVALID" ? "is-invalid" : "is-valid"}" required type="number" value="${this.model.value}" @input=${this.handleInput} @blur=${this.handleBlur}>
+                <button type="button" @click=${this.generateNumbers} tooltip="Generate random numbers" class="button -white -round -solid icon-only p-0 m-0 mx-0.5" style="width:36px;" flex="items-center justify-center">
+                    <svg class="m-0" style="width:16px;height:16px;" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
+                    </svg>
+                </button>
             </div>
         `;
         render(view, this);
@@ -139,6 +148,9 @@ class PasswordGenerator extends SuperComponent<PasswordGeneratorModel, PasswordG
             INPUT: {
                 NEXT: "OUTPUT",
             },
+            OUTPUT: {
+                RESTART: "SEPARATOR",
+            },
         }
         this.render();
     }
@@ -147,11 +159,16 @@ class PasswordGenerator extends SuperComponent<PasswordGeneratorModel, PasswordG
         this.update({inputs: this.model.inputs + 1});
     }
 
+    public removeInput(){
+        this.update({inputs: this.model.inputs - 1});
+    }
+
     private lookup(key:string): string{
         return diceware[key];
     }
 
-    private generate:EventListener = () => {
+    private generate:EventListener = (e:Event) => {
+        e.preventDefault();
         const updatedModel = {...this.model};
         updatedModel.words = [];
         const inputs:Array<InputComponent> = Array.from(this.querySelectorAll("input-component"));
@@ -172,35 +189,90 @@ class PasswordGenerator extends SuperComponent<PasswordGeneratorModel, PasswordG
         this.trigger("NEXT");
     }
 
+    private restart:EventListener = () => {
+        this.update({
+            inputs: 1,
+            words: [],
+            password: null,
+            separator: " ",
+        });
+        this.trigger("RESTART");
+    }
+
+    private copyToClipboard:EventListener = () => {
+        if ('clipboard' in navigator) {
+            navigator.clipboard.writeText(this.model.password).then(() => {});
+        } else {
+            const input = this.querySelector("input");
+            input.select();
+            input.setSelectionRange(0, 99999);
+            document.execCommand("copy");
+        }
+    }
+
     render(){
         let view;
         switch(this.state){
             case "SEPARATOR":
                 view = html`
-                    <label for="separator">Select A Separator:</label>
-                    <select @change=${this.setSeparator} required id="separator">
-                        <option value=" ">Space</option>
-                        <option value="-">Hyphen</option>
-                        <option value="_">Underscore</option>
-                        <option value="">No Separator</option>
-                    </select>
-                    <button @click=${this.next}>Next</button>
+                    <div class="select">
+                        <label for="separator">Select A Separator:</label>
+                        <select @change=${this.setSeparator} required id="separator">
+                            <option value=" ">Space</option>
+                            <option value="-">Hyphen</option>
+                            <option value="_">Underscore</option>
+                            <option value="">No Separator</option>
+                        </select>
+                        <i>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+                            </svg>
+                        </i>
+                    </div>
+                    <button class="block w-full button -solid -primary mt-1" @click=${this.next}>Next Step</button>
                 `;
                 break;
             case "INPUT":
                 view = html`
-                    ${Array.from(Array(this.model.inputs)).map(() => {
-                        return html`<input-component></input-component>`;
-                    })}
-                    <button @click=${this.addInput}>Add Input</button>
-                    <button @click=${this.generate}>Generate Password</button>
+                    <p class="block font-sm ${this.model.inputs < 5 ? "font-danger-600" : "font-grey-800"} text-center line-normal mb-1">For secure and rememberable passwords use 5 inputs.</p>
+                    <form @submit=${this.generate} grid="columns 1 gap-1">
+                        ${Array.from(Array(this.model.inputs)).map(() => {
+                            return html`<input-component></input-component>`;
+                        })}
+                        <div class="w-full" grid="columns 2 gap-1">
+                            <button type="button" class="button -solid -primary" @click=${this.addInput}>Add Input</button>
+                            <button type="submit" class="button -solid -success">Generate Password</button>
+                        </div>
+                    </form>
                 `;
                 break;
             case "OUTPUT":
-                view = html`<input type="text" readonly value="${this.model.password}">`;
+                view = html`
+                    <div class="input">
+                        <input type="text" readonly value="${this.model.password}" autofocus>
+                        <button @click=${this.copyToClipboard} class="button -grey -text -icon-only -round" tooltip="Copy to clipboard" flex="items-center justify-center" style="width:36px;height:36px;position:absolute;top:calc((100% - 36px) * 0.5);right:0.5rem">
+                            <svg style="width:18px;height:18px;" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2" />
+                            </svg>
+                        </button>
+                    </div>
+                    <button class="block w-full button -solid -primary mt-1" @click=${this.restart}>Restart</button>
+                `;
                 break;
         }
         render(view, this);
+        if (this.state === "OUTPUT"){
+            setTimeout(() => {
+                const input = this.querySelector("input");
+                if (input){
+                    input.focus();
+                    input.select();
+                }
+            }, 75);
+        }
     }
 }
+setInterval(() => {
+    refresh();
+}, 1000);
 customElements.define("password-generator", PasswordGenerator);
